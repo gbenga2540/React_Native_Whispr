8eb610cbae13f54eb038c1fc605e870667b0baaf
Akindeju Oluwagbemiga Michael <akindejuhmikel@gmail.com>
2024-06-11 16:08:49 +0100

    Fix: Icon for Android

--- android/app/src/main/java/com/baseapp/MainActivity.kt
+++ android/app/src/main/java/com/baseapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.baseapp
+package com.agma.whispr
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/baseapp/MainApplication.kt
+++ android/app/src/main/java/com/baseapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.baseapp
+package com.agma.whispr
 
 import android.app.Application
 import com.facebook.react.PackageList
